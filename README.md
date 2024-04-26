8dda875e0b432dc80d49450ff0c1f102fe6cd5b9
Ryan Hoegg <ryan.hoegg@gmail.com>
2024-04-26 12:39:31 -0500

    bootstrap the module

---
+++ README.md
@@ -0,0 +1,12 @@
+# Mule Commons Module
+
+Extras for Mule Development
+
+Add this dependency to your application pom.xml
+
+```
+<groupId>cloud.anypoint</groupId>
+<artifactId>mule-commons-module</artifactId>
+<version>0.0.0</version>
+<classifier>mule-plugin</classifier>
+```
---
+++ src/main/java/cloud/anypoint/commons/internal/CommonsOperations.java
@@ -0,0 +1,14 @@
+package cloud.anypoint.commons.internal;
+
+import static org.mule.runtime.extension.api.annotation.param.MediaType.ANY;
+
+import org.mule.runtime.extension.api.annotation.param.MediaType;
+import org.mule.runtime.extension.api.annotation.param.Config;
+import org.mule.runtime.extension.api.annotation.param.Connection;
+
+
+public class CommonsOperations {
+
+
+
+}
